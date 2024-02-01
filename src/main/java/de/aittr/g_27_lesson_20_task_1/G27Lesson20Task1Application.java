f83639496a54665fd77c8642c2cbfc4a7e5e9ecb
Vadim Shalamov <reetvvy@gmail.com>
2024-02-01 19:40:34 +0200

    начало разроботки домашнего задания lesson_20_task_1

---
+++ src/main/java/de/aittr/g_27_lesson_20_task_1/G27Lesson20Task1Application.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_lesson_20_task_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G27Lesson20Task1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G27Lesson20Task1Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/aittr/g_27_lesson_20_task_1/G27Lesson20Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_lesson_20_task_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G27Lesson20Task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
